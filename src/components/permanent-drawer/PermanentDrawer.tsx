a3398a6abe89770adcdca70071d4c1f419eb0cab
kerensp <105900888+kerensp@users.noreply.github.com>
2023-07-17 15:48:26 -0400

    crear menu lateral permanente

--- src/components/permanent-drawer/PermanentDrawer.tsx
+++ src/components/permanent-drawer/PermanentDrawer.tsx
@@ -1,16 +1,12 @@
-import * as React from 'react';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import {
-  Drawer,
-  List,
-  styled,
-  Button,
-  Typography,
-  Box,
-} from '@mui/material';
-import { ReactComponent as UserIcon } from '../../assets/icon/usuarios.svg';
+import { Drawer, List, styled, Button, Typography, Box } from '@mui/material';
+import { ReactComponent as UserIcon } from '../../assets/usuarios.svg';
+import { ReactComponent as RolIcon } from '../../assets/roles.svg';
+import { ReactComponent as TraceIcon } from '../../assets/trazas.svg';
+import { ReactComponent as AuditIcon } from '../../assets/auditoría.svg';
+import { ReactComponent as ExpedientIcom } from '../../assets/expedientes.svg';
+import { ReactComponent as WorkerIcon } from '../../assets/trabajadores.svg';
+import { ReactComponent as ProcessIcon } from '../../assets/procesos.svg';
 import logo from '../../assets/logo.svg';
-
 
 const drawerWidth = 280;
 
@@ -26,6 +22,7 @@ const StyledButton = styled(Button)({
   fontSize: '18px',
   paddingLeft: '23px',
   marginBottom: '8px',
+  gap: '.7rem',
   '&:hover': {
     background: '#4F2D80',
     color: '#FFFFFF',
@@ -39,11 +36,30 @@ const securityButtons: DrawerButton[] = [
   },
   {
     text: 'Roles',
-    icon: <InboxIcon />,
+    icon: <RolIcon />,
   },
   {
-    text: 'Configuración',
-    icon: <InboxIcon />,
+    text: 'Trazas',
+    icon: <TraceIcon />,
+  },
+  {
+    text: 'Auditoría',
+    icon: <AuditIcon />,
+  },
+];
+
+const rrhhButtons: DrawerButton[] = [
+  {
+    text: 'Expedientes',
+    icon: <ExpedientIcom />,
+  },
+  {
+    text: 'Trabajadores',
+    icon: <WorkerIcon />,
+  },
+  {
+    text: 'Procesos',
+    icon: <ProcessIcon />,
   },
 ];
 
@@ -59,15 +75,26 @@ export const PermanentDrawer = () => {
         },
       }}
       variant='permanent'>
+      <Box sx={{ w: '230px', h: '63px', margin: '40px 26.65px 40.49px 23px' }}>
+        <img src={logo} />
+      </Box>
 
-      <Box><img src={logo} /></Box>
-      
-      
-      <Typography color='#CBC7CF' ml='23px' mb='22px' fontSize='18px'>
+      <Typography color='#CBC7CF' ml='23px' mb='19px' fontSize='18px'>
         SEGURIDAD
       </Typography>
-      <List sx={{ alignContent: 'center', marginX: '23px', }}>
+      <List sx={{ alignContent: 'center', marginX: '23px' }}>
         {securityButtons.map((button, index) => (
+          <StyledButton key={index} startIcon={button.icon}>
+            {button.text}
+          </StyledButton>
+        ))}
+      </List>
+
+      <Typography color='#CBC7CF' ml='23px' mb='8px' mt='19px' fontSize='18px'>
+        RRHH
+      </Typography>
+      <List sx={{ alignContent: 'center', marginX: '23px' }}>
+        {rrhhButtons.map((button, index) => (
           <StyledButton key={index} startIcon={button.icon}>
             {button.text}
           </StyledButton>
