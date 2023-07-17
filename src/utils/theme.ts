5555e00723413a1a101b0295b599b831eb9c1ad4
kerensp <105900888+kerensp@users.noreply.github.com>
2023-07-17 07:19:52 -0400

    style: creating theme

---
+++ src/utils/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FFFFFF', // color primario
+      contrastText: '#4F2D80', // color para el texto contrastante
+    },
+  },
+  typography: {
+    fontFamily: ["Poppins", "Roboto", '"Segoe UI"'].join(","),
+  },
+});
